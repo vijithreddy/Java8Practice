cc8ab3e84d4757c0365666e8b0953303e5a9a34a
Vijith <b.vijithreddy@gmail.com>
2015-07-13 13:42:18 -0700

    Initial Commit

---
+++ Hammer.java
@@ -0,0 +1,14 @@
+
+public class Hammer {
+	int height;
+	Hammer nextHammer;
+	String name;
+	int age;
+	
+	public Hammer(int height, String name, int age){
+		this.height=height;
+		this.name=name;
+		this.age=age;
+	}
+
+}
